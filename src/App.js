b06ce8be1707300167d5aceff42340fe0746b0e9
Yousif Faraj <yfaraj96@gmail.com>
2020-11-29 20:08:57 -0500

    update project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
-import React from "react";
+import {React, useState} from "react";
 
-const App = () => <h1>Hello World</h1>;
-
+const App = () => {
+  const [name, setName] = useState('World');
+  const [tempName, setTempName] = useState('');
+  return (
+    <div>
+      <h1>Hello {name}</h1>
+      <label>
+        Name:
+        <input type="text" onChange={(e) => setTempName(e.target.value)} />
+      </label>
+      <button onClick={() => setName(tempName)}>Change Name</button>
+    </div>
+  );
+};
 export default App;
